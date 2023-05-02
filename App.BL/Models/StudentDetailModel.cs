26055a70aa67d4b14045a44812ac6e2c72b8167e
Daniel Gomez <daniel.gomez@verndale.com>
2023-05-02 14:05:28 -0500

    NET7

---
+++ App.BL/Models/StudentDetailModel.cs
@@ -0,0 +1,19 @@
+﻿using DotVVM.AutoUI.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.BL.Models;
+
+public class StudentDetailModel
+{
+    [Visible(ViewNames = "List")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "First name is required!")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is required!")]
+    public string LastName { get; set; } = null!;
+
+    public DateTime EnrollmentDate { get; set; }
+    [DataType(DataType.MultilineText)]
+    public string About { get; set; } = null!;
+}
---
+++ App.BL/Models/StudentListModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DotVVM.AutoUI.Annotations;
+
+namespace App.BL.Models;
+
+public class StudentListModel
+{
+    [Display(Name = "User ID")]
+    [Visible(ViewNames = "List")]
+    public int Id { get; set; }
+
+    [Display(Name = "Name", AutoGenerateField = true)]
+    public string FirstName { get; set; } = null!;
+    [Display(Name = "Last Name", AutoGenerateField = true)]
+    public string LastName { get; set; } = null!;
+}
